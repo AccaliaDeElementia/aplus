8e666747cd3205d32d97f43b442d1345aae47c4b
Accalia de Elementia <accalia.de.elementia@gmail.com>
2015-09-16 00:39:40 +0000

    add support for enumerating objects to aplus.each and friends

--- test/lib/aplus-each.js
+++ test/lib/aplus-each.js
@@ -153,6 +153,153 @@ describe('aplus.eachLimit', function () {
         });
     });
 });
+describe('aplus.eachLimit() {object}', function () {
+    it('should accept empty object values', function () {
+        var input = {};
+        return aplus.eachLimit(input, 5, function (i) {
+            return Promise.resolve(i * i);
+        });
+    });
+    it('should map object values', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = {
+                first: 1,
+                second: 4,
+                third: 9
+            };
+        return aplus.eachLimit(input, 5, function (i) {
+            return Promise.resolve(i * i);
+        }).then(function (result) {
+            result.should.eql(expected);
+        });
+    });
+
+    it('should accept non number limit', function () {
+        var input = {
+            first: 1,
+            second: 2,
+            third: 3
+        };
+
+        function square(i) {
+            return Promise.resolve(i * i);
+        }
+        return aplus.eachLimit(input, undefined, square);
+    });
+    it('should reject on inner promise rejection', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = new Error('i am error' + Math.random());
+
+        function square(i) {
+            return new Promise(function (resolve, reject) {
+                if (i === 2) {
+                    reject(expected);
+                } else {
+                    resolve(i * i);
+                }
+            });
+        }
+        return aplus.eachLimit(input, 4, square).then(function () {
+            chai.assert.fail('Should not accept promise');
+        }).catch(function (rejection) {
+            rejection.should.equal(expected);
+        });
+    });
+    it('should reject on inner promise thrown error', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = new Error('i am error' + Math.random());
+
+        function square(i) {
+            return new Promise(function (resolve) {
+                if (i === 3) {
+                    throw expected;
+                } else {
+                    resolve(i * i);
+                }
+            });
+        }
+        return aplus.eachLimit(input, 3, square).then(function () {
+            chai.assert.fail('Should not accept promise');
+        }).catch(function (rejection) {
+            rejection.should.equal(expected);
+        });
+    });
+    it('should not spawn promises after rejection occurs', function () {
+        var input = {},
+            promises = 0,
+            length = 100;
+        for (var i = 1; i <= length; i += 1) {
+            input['a' + i] = i;
+        }
+
+        function filter(j) {
+            promises += 1;
+            if (j === 10) {
+                return Promise.reject(new Error('boo'));
+            }
+            return Promise.resolve(true);
+        }
+        return aplus.eachLimit(input, 3, filter).catch(function () {
+            promises.should.be.below(length);
+        });
+    });
+    it('should spawn only as many Promises as input items', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            promises = 0;
+
+        function square(i) {
+            return new Promise(function (resolve) {
+                promises += 1;
+                resolve(i * i);
+
+            });
+        }
+        return aplus.eachLimit(input, 100, square).then(function () {
+            promises.should.equal(3);
+        });
+    });
+    describe('should spawn only up to the provided limit of promises', function () {
+        var input = {},
+            length = 100;
+        for (var i = 1; i <= length; i += 1) {
+            input['a' + i] = i;
+        }
+        Object.keys(input).forEach(function (key) {
+            var limit = input[key];
+            it('limit: ' + limit, function () {
+                var activePromises = 0;
+
+                function square(j) {
+                    return new Promise(function (resolve) {
+                        activePromises++;
+                        activePromises.should.be.at.most(limit);
+                        resolve(j * j);
+                    }).then(function (value) {
+                        activePromises--;
+                        return Promise.resolve(value);
+                    });
+                }
+                return aplus.eachLimit(input, limit, square);
+            });
+        });
+    });
+});
 describe('aplus.eachSeries', function () {
     var sandbox;
     beforeEach(function () {
@@ -187,7 +334,135 @@ describe('aplus.eachSeries', function () {
         aplus.eachLimit.firstCall.args[2].should.equal(iterator);
     });
 });
+describe('aplus.eachSeries() {object}', function () {
+    it('should accept empty object values', function () {
+        var input = {};
+        return aplus.eachSeries(input, function (i) {
+            return Promise.resolve(i * i);
+        });
+    });
+    it('should map object values', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = {
+                first: 1,
+                second: 4,
+                third: 9
+            };
+        return aplus.eachSeries(input, function (i) {
+            return Promise.resolve(i * i);
+        }).then(function (result) {
+            result.should.eql(expected);
+        });
+    });
+    it('should reject on inner promise rejection', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = new Error('i am error' + Math.random());
 
+        function square(i) {
+            return new Promise(function (resolve, reject) {
+                if (i === 2) {
+                    reject(expected);
+                } else {
+                    resolve(i * i);
+                }
+            });
+        }
+        return aplus.eachSeries(input, square).then(function () {
+            chai.assert.fail('Should not accept promise');
+        }).catch(function (rejection) {
+            rejection.should.equal(expected);
+        });
+    });
+    it('should reject on inner promise thrown error', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = new Error('i am error' + Math.random());
+
+        function square(i) {
+            return new Promise(function (resolve) {
+                if (i === 3) {
+                    throw expected;
+                } else {
+                    resolve(i * i);
+                }
+            });
+        }
+        return aplus.eachSeries(input, square).then(function () {
+            chai.assert.fail('Should not accept promise');
+        }).catch(function (rejection) {
+            rejection.should.equal(expected);
+        });
+    });
+    it('should not spawn promises after rejection occurs', function () {
+        var input = {},
+            promises = 0,
+            length = 100;
+        for (var i = 1; i <= length; i += 1) {
+            input['a' + i] = i;
+        }
+
+        function filter(j) {
+            promises += 1;
+            if (j === 10) {
+                return Promise.reject(new Error('boo'));
+            }
+            return Promise.resolve(true);
+        }
+        return aplus.eachSeries(input, filter).catch(function () {
+            promises.should.equal(10);
+        });
+    });
+    it('should spawn only as many Promises as input items', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            promises = 0;
+
+        function square(i) {
+            return new Promise(function (resolve) {
+                promises += 1;
+                resolve(i * i);
+
+            });
+        }
+        return aplus.eachSeries(input, square).then(function () {
+            promises.should.equal(3);
+        });
+    });
+    it('should spawn only up to the provided limit of promises', function () {
+        var input = {},
+            activePromises = 0,
+            length = 100;
+        for (var i = 1; i <= length; i += 1) {
+            input['a' + i] = i;
+        }
+
+        function square(j) {
+            return new Promise(function (resolve) {
+                activePromises++;
+                activePromises.should.be.at.most(1);
+                resolve(j * j);
+            }).then(function (value) {
+                activePromises--;
+                return Promise.resolve(value);
+            });
+        }
+        return aplus.eachSeries(input, square);
+    });
+});
 describe('aplus.each', function () {
     var sandbox;
     beforeEach(function () {
@@ -220,5 +495,77 @@ describe('aplus.each', function () {
         var iterator = sinon.spy();
         aplus.each([], iterator);
         aplus.eachLimit.firstCall.args[2].should.equal(iterator);
+    });
+});
+
+describe('aplus.each() {object}', function () {
+    it('should accept empty object values', function () {
+        var input = {};
+        return aplus.each(input, function (i) {
+            return Promise.resolve(i * i);
+        });
+    });
+    it('should map object values', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = {
+                first: 1,
+                second: 4,
+                third: 9
+            };
+        return aplus.each(input, function (i) {
+            return Promise.resolve(i * i);
+        }).then(function (result) {
+            result.should.eql(expected);
+        });
+    });
+    it('should reject on inner promise rejection', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = new Error('i am error' + Math.random());
+
+        function square(i) {
+            return new Promise(function (resolve, reject) {
+                if (i === 2) {
+                    reject(expected);
+                } else {
+                    resolve(i * i);
+                }
+            });
+        }
+        return aplus.each(input, square).then(function () {
+            chai.assert.fail('Should not accept promise');
+        }).catch(function (rejection) {
+            rejection.should.equal(expected);
+        });
+    });
+    it('should reject on inner promise thrown error', function () {
+        var input = {
+                first: 1,
+                second: 2,
+                third: 3
+            },
+            expected = new Error('i am error' + Math.random());
+
+        function square(i) {
+            return new Promise(function (resolve) {
+                if (i === 3) {
+                    throw expected;
+                } else {
+                    resolve(i * i);
+                }
+            });
+        }
+        return aplus.each(input, square).then(function () {
+            chai.assert.fail('Should not accept promise');
+        }).catch(function (rejection) {
+            rejection.should.equal(expected);
+        });
     });
 });
